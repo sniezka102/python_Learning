aff5cd6ace2d4da537c55d63526e276417c7e15f
Ann <anna.szmyt.home@gmail.com>
2017-05-22 14:30:17 +0200

    Update task05.py
    new webpage was added

--- task05.py
+++ task05.py
@@ -10,6 +10,8 @@ printString: to print the string in upper case.
 Also please include simple test function to test the class methods.
 
 https://jeffknupp.com/blog/2014/06/18/improve-your-python-python-classes-and-object-oriented-programming/ - read more
+https://learnpythonthehardway.org/book/ex40.html
+
 """
 
 
