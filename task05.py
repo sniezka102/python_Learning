a866ae7cdd05b09d16c6643480ef202c6cc35fdd
sniezka102 <anna.szmyt.home@gmail.com>
2017-05-23 22:24:48 +0200

    function modification

--- task05.py
+++ task05.py
@@ -7,16 +7,24 @@ getString: to get a string from console input
 printString: to print the string in upper case.
 Also please include simple test function to test the class methods.
 
+https://jeffknupp.com/blog/2014/06/18/improve-your-python-python-classes-and-object-oriented-programming/ - read more
+https://learnpythonthehardway.org/book/ex40.html
+
 """
-class GetInputFromTheConsole:
-  def getString(self):
-    self.data = input("give the string")
-    self.printString()
 
-  def printString(self):
-    print (self.data.upper())
-  
-    
-newObj = GetInputFromTheConsole()
+
+class InputStringToUppercaseConverter:
+    def __init__(self):
+        self.data = "" #now the string data was created, it will be initialized
+                        #all the time when new object was created
+
+    def getString(self):
+        self.data = input("give the string ")
+
+    def printString(self):
+        print (self.data.upper())
+
+
+newObj = InputStringToUppercaseConverter()
 newObj.getString()
-
+newObj.printString()
