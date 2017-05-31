571aa14a4bafc2ce4669c8b8bbc908d535abde27
Ann <anna.szmyt.home@gmail.com>
2017-05-31 14:39:37 +0200

    the function replace was used to changed value 'a' on the digit value

--- task15.py
+++ task15.py
@@ -5,13 +5,26 @@ Write a program that computes the value of a+aa+aaa+aaaa with a given digit as t
 	9
 Then, the output should be:
 	11106
-	
 Hints:
 In case of input data being supplied to the question, it should be assumed to be a console input.
 
 """
+a = input ("write one value")
+sumcia = 0
+swappedValues = []
 
-a = input ("add the value")
-a = int(a)
-computedValue = a + a*a + a*a*a + a*a*a*a
-print (computedValue)
+formula = "a+aa+aaa+aaaa"
+for item in formula:
+  if item == 'a':
+    swappedValues = formula.replace(item, a)
+  else:
+    pass
+
+formula = (str(swappedValues)).split("+")
+
+for item in formula:
+  sumcia += int(item)
+
+
+print(swappedValues)
+print(sumcia)
