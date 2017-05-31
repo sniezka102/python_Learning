10e2f93e22c7d1988069d6c9b8fd066c619ad75f
Ann <anna.szmyt.home@gmail.com>
2017-05-31 22:45:00 +0200

    Update task14.py

--- task14.py
+++ task14.py
@@ -2,7 +2,8 @@
 
 Question 14 -- Level 2
 Question:
-Write a program that accepts a sentence and calculate the number of upper case letters and lower case letters. Suppose the following input is supplied to the program:
+Write a program that accepts a sentence and calculate the number of upper case letters and lower case letters. 
+Suppose the following input is supplied to the program:
 	Hello world!
 Then, the output should be:
 	UPPER CASE 1
@@ -13,17 +14,17 @@ In case of input data being supplied to the question, it should be assumed to be
 """
 
 inputSentence = input("write some text")
-countsUpperLetters = 0 
-countsLowerLetters = 0 
-for items in inputSentence:
-  if items.isupper():  #function which give possibility to check if current letter is an upper letter
-    countsUpperLetters += 1
-  elif items.islower():  #function which give possibility to check if current letter is a lower letter 
-    countsLowerLetters +=1
+upperLettersCount = 0 
+lowerLettersCount = 0 
+for character in inputSentence:
+  if character.isupper():  #function which give possibility to check if current letter is an upper letter
+    upperLettersCount += 1
+  elif character.islower():  #function which give possibility to check if current letter is a lower letter 
+    lowerLettersCount +=1
   else:
     pass  #I need to add the else statement but I didn't have any sentence to use here, that's why I used  the pass statement
 
-print(" UPPER CASE ", countsUpperLetters, "\n", "LOWER CASE ", countsLowerLetters)
+print(" UPPER CASE ", upperLettersCount, "\n", "LOWER CASE ", lowerLettersCount)
 
 
 """
@@ -31,5 +32,6 @@ Pass statement:
 
 It is used when a statement is required syntactically but you do not want any command or code to execute.
 
-The pass statement is a null operation; nothing happens when it executes. The pass is also useful in places where your code will eventually go, but has not been written yet
+The pass statement is a null operation; nothing happens when it executes. The pass is also useful in places where your code 
+will eventually go, but has not been written yet
 """
