24511f87de353c91c2b10eeff8d7575e39cfa269
Ann <anna.szmyt.home@gmail.com>
2017-05-31 22:34:55 +0200

    Update task12.py
    now the input is not taken from the console

--- task12.py
+++ task12.py
@@ -1,27 +1,25 @@
 """
 Question 12 -- Level 2
 Question:
-Write a program, which will find all such numbers between 1000 and 3000 (both included) such that each digit of the number is an even number. The numbers obtained should be printed in a comma-separated sequence on a single line.
+Write a program, which will find all such numbers between 1000 and 3000 (both included) such that each digit of the number 
+is an even number. The numbers obtained should be printed in a comma-separated sequence on a single line.
 Hints:
 In case of input data being supplied to the question, it should be assumed to be a console input.
 
 
 """
-
-
-wholeEvenNumbersInNumber = []
-numb = input("add value in range <1000,3001>")
-if (int(numb) >= 1000) and (int(numb) <= 3000):
-  if ((int(numb[0])%2)==0) and ((int(numb[1])%2)==0) and ((int(numb[2])%2)==0) and ((int(numb[3])%2)==0):
-    wholeEvenNumbersInNumber.append(numb)
+numbersWithOnlyEvenDigits = []
+for item in range (1000,3001):
+  number = str(item)
+  if ((int(number[0])%2)==0) and ((int(number[1])%2)==0) and ((int(number[2])%2)==0) and ((int(number[3])%2)==0):
+    numbersWithOnlyEvenDigits.append(number)
   else:
-    print("non all of the numbers in number are even")
-else:
-  print("number = ", num, " is out of range")
-print (",".join(wholeEvenNumbersInNumber))
+    pass
+print (",".join(numbersWithOnlyEvenDigits))
   
 """
-current solution obtain hints that data should have been taken from the console. In this case not possible to separate by .join
+current solution obtain hints that data should have been taken from the console. In this case not possible to separate 
+by .join
 
 bellow I will create the solution when few numbers were added from the console
 
