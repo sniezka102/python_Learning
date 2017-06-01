9c0b8eebab943cfb2298c4c69608ae9ac4923bb2
Ann <anna.szmyt.home@gmail.com>
2017-06-01 15:11:15 +0200

    Update task_10.py
    added command about the:
    - split() method which help to split  string by usuing the special charakter, in case of white charakter is possible to use the commend .split(" ")
    - about utility, sorry but I don't understand what exactly do you mean. We need to discus :)

--- task_10.py
+++ task_10.py
@@ -19,13 +19,13 @@ lines = []
 print("add the text line, write 'break' if you want to finish action\n")
 while (True):
   inputString = input()
-  if inputString == 'break':    #?????????
-    break;                      #????????
-  else:
+  if inputString:
     lines.append(inputString)
+   else:
+    break
 
 
-lines = "".join(lines) #The method join() returns a string in which the string elements of sequence                     #have been joined by str separator.
+lines = "".join(lines) #The method join() returns a string in which the string elements of sequence                    
 words = lines.split(" ")
 
 allWords = set(words) #Test if every element in the set is in other.
@@ -35,15 +35,20 @@ print(" ".join(sorted(allWords))) #sortowanie
 """
 Maćka sugestie do zmian:
 task 10
-- co robią pliki task_08.py i task_09.py na tym branchu?
+
 L20 to jest niepotrzebne, w treści nie ma nic o tym, że to ma być w pętli
 L29 widzę, że 'oryginalne' rozwiązanie jest takie samo, ale w treści jest
     że mają być rozdzielone przez whitespace character. Whitespace to też
     na przykład tabulacja.
     https://pl.wikipedia.org/wiki/Znaki_niedrukowalne
     Czy jest w pythonie:
-    a) wariant split, który przyjmuje set/array znaków, które są "dzielącymi"?
-    b) jakieś utility (coś na stringu?), które zwraca whitecharactersSet?
+    a) wariant split, który przyjmuje set/array znaków, które są "dzielącymi"? - w zadaniu jest zastosowane metoda podziału 
+                                                                                całego tekstu poprzez usunięcie znaków ich dzielących
+                                                                                w przypadku białych znaków jest to split(" ")
+                                                                              
+    b) jakieś utility (coś na stringu?), które zwraca whitecharactersSet? - nie wiem czy dokładnie o to chodzi, ale jest metoda sprawdzająca 
+                                                                            czy dany string jest white, jest to .isspace()
+                                                                            
 
 
 """
