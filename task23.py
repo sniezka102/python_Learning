c6a3d75d2690a6890ea1dc94f3880ac707019d96
Ann <anna.szmyt.home@gmail.com>
2017-06-21 15:21:19 +0200

    task completed

---
+++ task23.py
@@ -0,0 +1,12 @@
+"""
+Question 23 - level 1
+Question:
+   Write a method which can calculate square value of number
+Hints:
+    Using the ** operator
+
+"""
+def calculateSquareOfNumber(n):
+    return (n**2)
+
+print(calculateSquareOfNumber(2))
