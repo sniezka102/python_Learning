5322b0959a7b69e0a5767e2c23339ce176511c05
sniezka102 <anna.szmyt.home@gmail.com>
2017-06-22 22:31:50 +0200

    task completed

---
+++ task29.py
@@ -0,0 +1,15 @@
+"""
+Question 29
+Define a function that can accept two strings as input
+and concatenate them and then print it in console.
+Hints:
+Use + to concatenate the strings
+"""
+
+def concatenateTwoString(string1, string2):
+    return (string1 + string2)
+
+text1 = 'siala baba mak'
+text2 = ' nie wiedziala jak'
+addedTwoStrings = concatenateTwoString(text1,text2)
+print(addedTwoStrings)
