f2c8eb10a56becb0b68226c2c4da66748e52f834
Ann <anna.szmyt.home@gmail.com>
2017-06-22 21:49:55 +0200

    Create task26.py
    task completed

---
+++ task26.py
@@ -0,0 +1,14 @@
+"""
+Question 26
+
+Define a function which can compute the sum of two numbers.
+
+Hints:
+Define a function with two numbers as arguments.
+You can compute the sum in the function and return the value.
+
+"""
+def computeTwoValues(a,b):
+    return (a + b)
+
+print(computeTwoValues(10,20))
